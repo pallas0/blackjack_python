74240b92bc55e656803d665001ac897846a4ba3c
pallas0 <kablooigrl@gmail.com>
2020-09-07 19:12:15 -0400

    division into stages and class expansion

--- Classes.py
+++ Classes.py
@@ -1,11 +1,38 @@
-import sys, pygame, random
+import sys, pygame, random, math
 from pygame.locals import *
+import pygame_gui
+
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
 
 class Img:
 	def __init__(self, obj, x, y):
 		self.obj = obj
 		self.x = x
 		self.y = y
+
+	def move(fill_func, blit_func, screen_update_func,
+	 screen_obj):
+		def move_func(self, new_x, new_y):
+			dx, dy = -1, -1
+			if abs(new_x - self.x) >= 1:
+				self.x += dx*4
+				if abs(new_y - self.y) >= 1:
+					self.y += dy*4
+					fill_func(screen_obj)
+			blit_func(self.obj, (self.x, self.y))
+			screen_update_func()
+			return (self.x, self.y)
+		return move_func
+				# screen.fill(WHITE)
+		# while abs(new_x - x) >= 0.001:
+
+		#  	() abs(new_x - x) > 0.001:
+		# 	x += dy_dx
+		# 	while abs(new_y - y) > 0.001:
+		# 		y += dy_dx
+
+
 
 
 class Card:
@@ -27,3 +54,5 @@ class Deck:
 		for val in [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]:
 			for suit_elem in ["hearts", "spades", "clubs", "diamonds"]:
 				self.cards.append(Card(val, suit_elem))
+
+
--- Deck.py
+++ Deck.py
@@ -2,6 +2,7 @@ import sys, pygame, random
 from pygame.locals import *
 import pygame_gui
 from Classes import Img
+
 #screen set up
 
 pygame.init()
@@ -41,12 +42,13 @@ hello_button = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((350, 275)
 # goodbye_button = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((350, 375), (100, 50)),
 # 	text='Say Goodbye', manager=manager)
 
+#intro stage bool
+intro = True
+main = False
 
 #event [while] loop
-
 while is_running:
 	time_delta = clock.tick(60)/1000.0
-	intro = True
 
 
 	for event in pygame.event.get():
@@ -58,6 +60,8 @@ while is_running:
 				if event.ui_element == hello_button:
 					print('Hello World!')
 					print(hello_button.check_pressed())
+					intro = False
+					main = True
 					img0.x, img0.y = 800, 600
 					screen.fill(WHITE)
 					screen.blit(background, (0, 0))
@@ -66,18 +70,45 @@ while is_running:
 		manager.process_events(event)
 
 	manager.update(time_delta)
+	
+	while intro:
+		
+		manager.draw_ui(screen)
+		move0 = Img.move(screen.fill, screen.blit, pygame.display.update, WHITE)
+		(n, m) = move0(img0, 40, 100)
+		print(n, m)
+		manager.process_events(event)
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				intro, is_running = False, False
+			if event.type == pygame.USEREVENT:
+				screen.fill(WHITE)
+				pygame.display.update()
+				main = True
+				intro = False
+		manager.update(time_delta)
+		pygame.display.update()
+
+	while main:
+		screen.fill(WHITE)
+		manager.process_events(event)
+		
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				main, is_running = False, False
+		manager.update(time_delta)
+
+	# if img0.x != 40:
+	# 	img0.x -= 4
+	# 	if img0.y != 100:
+	# 		img0.y -= 4
+	# 		screen.fill(WHITE)
 
 	
-	if img0.x != 40:
-		img0.x -= 4
-		if img0.y != 100:
-			img0.y -= 4
-			screen.fill(WHITE)
-
-	
-	screen.blit(img0.obj, (img0.x, img0.y))
-	screen.blit(img_intro_text.obj, (img_intro_text.x, img_intro_text.y))
-	manager.draw_ui(screen)
+	# screen.blit(img0.obj, (img0.x, img0.y))
+	# screen.blit(img_intro_text.obj, (img_intro_text.x, img_intro_text.y))
+		manager.draw_ui(screen)
+		pygame.display.update()
 
 	
 	pygame.display.update()
